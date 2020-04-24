44714c5bb5b36593367f588ec642c0b9b096d1e2
Hamza <hamzadanden2@gmail.com>
2020-04-24 13:13:00 +0200

    implementing error handling

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,26 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
 
+// Routes
 app.use("/api/items", itemsRouter);
 
+app.all("*", (req, res, next) => {
+  //   res.status(404).json({
+  //     status: "fail",
+  //     message: `Can't find ${req.originalUrl} on this server`,
+  //   });
+  const err = new Error();
+  err.status = "fail";
+  err.statusCode = 404;
+  err.message = `Can't find ${req.originalUrl} on this server`;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 module.exports = app;
